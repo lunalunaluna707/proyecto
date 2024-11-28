a3e3f752f2140f6eb71f77711baef392b5d09da5
lunalunaluna707 <vazquezmariana125670@gmail.com>
2024-11-28 12:09:54 -0600

    Update empleadoDAO.py

--- empleadoDAO.py
+++ empleadoDAO.py
@@ -1,43 +1,45 @@
 import mysql.connector
 
-class Empleado:
-    def __init__(self,host,database,user,password):
-        self.config={
-            'host':host,
-            'database':database,
-            'user':user,
-            'password':password
+class EmpleadoDAO:
+    def __init__(self, host, database, user, password):
+        self.config = {
+            'host': host,
+            'database': database,
+            'user': user,
+            'password': password
         }
+
     def obtener_datos(self):
-            try:
-                con = mysql.connector.connect(**self.config)
-                cursor = con.cursor()
-                cursor.execute("SELECT * FROM empleados")    
-                registros = cursor.fetchall()
-                print("Registros obtenidos:", registros)
-            except mysql.connector.Error as err:
-                print("Error al conectar con MySQL:", err)
-                registros = []  
-            finally:
-                if con.is_connected():
-                    cursor.close()
-                    con.close()
-            return registros 
-    def insertar(self,empleado):
         try:
             con = mysql.connector.connect(**self.config)
             cursor = con.cursor()
-            sql = "INSERT INTO empleados (Nombre_Empleado, Numero_Empleado, Telefono, Domicilio, CURP, NSS, RFC, Correo) VALUES (%s, %s, %s,%s, %s, %s,%s, %s)"
+            cursor.execute("SELECT * FROM empleados")
+            registros = cursor.fetchall()
+            return registros
+        except mysql.connector.Error as err:
+            print("Error al conectar con MySQL:", err)
+            return []
+        finally:
+            if con.is_connected():
+                cursor.close()
+                con.close()
+
+    def insertar(self, empleado):
+        try:
+            con = mysql.connector.connect(**self.config)
+            cursor = con.cursor()
+            sql = "INSERT INTO empleados (Nombre_Empleado, Numero_Empleado, Telefono, Domicilio, CURP, NSS, RFC, Correo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
             cursor.execute(sql, empleado)
             con.commit()
             return cursor.rowcount
         except mysql.connector.Error as err:
-            print("error al conectar msql:", err)
+            print("Error al insertar:", err)
             return 0
         finally:
             if con.is_connected():
                 cursor.close()
                 con.close()
+
     def eliminar(self, id_empleado):
         try:
             con = mysql.connector.connect(**self.config)
@@ -47,7 +49,7 @@ class Empleado:
             con.commit()
             return cursor.rowcount
         except mysql.connector.Error as err:
-            print("error al eliminar:", err)
+            print("Error al eliminar:", err)
             return 0
         finally:
             if con.is_connected():
