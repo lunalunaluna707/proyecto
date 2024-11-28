3488416a4e3f591de8c11e1a9f93cbc285d8a858
lunalunaluna707 <vazquezmariana125670@gmail.com>
2024-11-28 12:12:37 -0600

    Add files via upload

---
+++ empleado.py
@@ -0,0 +1,10 @@
+class Empleado:
+    def __init__(self, nombre, numero, telefono, domicilio, curp, nss, rfc, correo):
+        self.nombre = nombre
+        self.numero = numero
+        self.telefono = telefono
+        self.domicilio = domicilio
+        self.curp = curp
+        self.nss = nss
+        self.rfc = rfc
+        self.correo = correo
